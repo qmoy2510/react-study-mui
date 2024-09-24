79d005edbab76e236def8292c842421eed498d15
Shuung <qmoy2510@gmail.com>
2024-09-24 13:32:32 +0900

    feat: counter 추가

--- src/app/index/page.tsx
+++ src/app/index/page.tsx
@@ -1,9 +1,22 @@
-import { Stack, Typography } from '@mui/material';
+import { Button, Stack, Typography } from '@mui/material';
+import { useState } from 'react';
 
 export default function IndexPage() {
+  const [count, setCount] = useState(0);
+
+  function handleButtonClick(){
+    setCount(prev=>prev+1);
+  }
+  function handleButtonClickReset(){
+    setCount(0);
+  }
+
   return (
     <Stack>
       <Typography>Index Page</Typography>
+      <Typography variant='h1'>{count}</Typography>
+      <Button onClick={handleButtonClick} variant='contained'>count</Button>
+      <Button onClick={handleButtonClickReset} variant='contained'>reset</Button>
     </Stack>
   );
 }
